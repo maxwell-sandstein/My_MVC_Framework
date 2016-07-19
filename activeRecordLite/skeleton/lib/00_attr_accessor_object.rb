a336d92f8fcf6b1ed6f402c7b014a000dbc6b693
Oscar <osandoval1618@gmail.com>
2016-07-19 13:05:21 -0700

    table names class methods done.  Attr accessor check

--- activeRecordLite/skeleton/lib/00_attr_accessor_object.rb
+++ activeRecordLite/skeleton/lib/00_attr_accessor_object.rb
@@ -1,5 +1,45 @@
+require('byebug')
+
 class AttrAccessorObject
-  def self.my_attr_accessor(*names)
-    # ...
+  def self.my_attr_accessor(*names) #protect against non symbols in names
+    names.each do |name|
+      raise 'invalid argument type' unless name.is_a?(Symbol)
+      self.define_getter(name)
+      self.define_setter(name)
+    end
+  end
+
+  private
+
+  def self.define_getter(name)
+    define_method(name) do
+      instance_variable = "@#{name.to_s}"
+      self.instance_variable_get(instance_variable)
+    end
+  end
+
+  def self.define_setter(name)
+     name_with_equals = ("#{name.to_s}=")
+     name_with_equals = name_with_equals.to_sym
+     instance_variable = "@#{name.to_s}"
+
+     define_method(name_with_equals) do |value|
+       self.instance_variable_set(instance_variable, value)
+     end
   end
 end
+
+
+# # test case
+# class Inheriting < AttrAccessorObject
+#   my_attr_accessor :x, :y
+#
+#   def initialize(x, y)
+#     self.x = x
+#     self.y = y
+#   end
+# end
+#
+# test = Inheriting.new('success', 'supersuccess')
+# puts test.x
+# puts test.y
--- activeRecordLite/skeleton/lib/01_sql_object.rb
+++ activeRecordLite/skeleton/lib/01_sql_object.rb
@@ -3,6 +3,8 @@ require 'active_support/inflector'
 # NB: the attr_accessor we wrote in phase 0 is NOT used in the rest
 # of this project. It was only a warm up.
 
+require 'byebug'
+
 class SQLObject
   def self.columns
     # ...
@@ -12,11 +14,34 @@ class SQLObject
   end
 
   def self.table_name=(table_name)
-    # ...
+    @table_name = table_name
   end
 
   def self.table_name
-    # ...
+    return @table_name unless @table_name.nil?
+
+    self.deduce_table_name
+  end
+
+  def self.deduce_table_name
+    model_name = self.to_s
+    capitals = ("A".."Z").to_a
+    capital_indices = []
+
+    model_name.length.times do |letterIdx|
+      capital_indices << letterIdx if capitals.include?(model_name[letterIdx])
+    end
+
+    capital_indices << model_name.length
+
+    words = []
+    capital_indices.each_with_index do |capitalIdx, i|
+      next if i === capital_indices.length - 1
+      nextCapitalIdx = capital_indices[i + 1]
+      words << model_name[capitalIdx...nextCapitalIdx].downcase
+    end
+
+    words.join('_')
   end
 
   def self.all
@@ -55,3 +80,14 @@ class SQLObject
     # ...
   end
 end
+
+
+
+
+#tests
+class BigDog < SQLObject
+
+end
+
+puts BigDog.table_name
+#table names work
